7510a8daf5ad52737df92e6e609e93beb384dafa
Mateo Salcedo <m.salcedo@uniandes.edu.co>
2019-11-18 13:39:16 -0500

    Ejercicio

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {FormattedDate} from 'react-intl';
+import {FormattedNumber} from 'react-intl';
+import {FormattedPlural} from 'react-intl';
+import {FormattedMessage} from 'react-intl';
 
 export default class Job extends React.Component {
 
@@ -8,9 +12,28 @@ export default class Job extends React.Component {
   				<th scope="row">{this.props.offer.id}</th>
       			<td>{this.props.offer.name}</td>
   				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
+      			<td>{this.props.offer.salary} &nbsp;
+					<FormattedPlural
+						value = {this.props.offer.salary}
+						one = <FormattedMessage id="One"/>
+						other = <FormattedMessage id="Other"/>
+					/>
+				</td>
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+				<td>
+					<FormattedDate
+						value={new Date(this.props.offer.date)}
+						year='numeric'
+						month='long'
+						day='numeric'
+						weekday='long'
+					/>
+				</td>
+				<td>
+					<FormattedNumber
+						value = {this.props.offer.views}
+					/>
+				</td>
   			</tr>
   		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+ReactDOM.render(
+    <IntlProvider locale={navigator.language} messages= {navigator.language === 'es' ? localeEsMessages:localeEnMessages}>
+        <JobsList/>
+        {console.log(navigator.language)}
+    </IntlProvider>, document.getElementById("root")
+);
